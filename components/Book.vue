ef6d191354676efadfb20acfb91225ca16784308
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-03-01 20:18:39 +0000

    UI – Create book component with 3D shine effect

---
+++ components/Book.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="rounded-lg overflow-hidden shadow-lg relative h-[300px] w-[180px] group hover:scale-105 transition-transform duration-300"
+  >
+    <div
+      v-for="i in [0, 1, 2, 3, 4]"
+      class="absolute top-0 w-full bg-[#BFAD9E] rounded-lg shadow shadow-stone-800/30"
+      :style="{
+        height: `${100 - i}%`,
+      }"
+    ></div>
+    <div
+      class="absolute top-0 w-full h-[95%] rounded-lg shadow shadow-stone-800/30 overflow-hidden"
+    >
+      <img
+        :src="src"
+        alt="Book"
+        class="absolute w-full h-full rounded-lg shadow shadow-stone-800/30 object-cover"
+      />
+      <div
+        class="absolute left-0 h-full bg-gradient-to-r from-white/50 to-transparent w-6 group-hover:w-12 transition-all duration-400"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string;
+}
+
+defineProps<Props>();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-    Home page
+  <div class="p-12 flex flex-row gap-12">
+    <Book
+      src="https://m.media-amazon.com/images/I/81j8aBGFBqL._AC_UF894,1000_QL80_.jpg"
+    />
+    <Book
+      src="https://m.media-amazon.com/images/I/81gUZu+VSOL._AC_UF894,1000_QL80_.jpg"
+    />
+    <Book
+      src="https://m.media-amazon.com/images/I/81yTR5tlhEL._AC_UF894,1000_QL80_.jpg"
+    />
+    <Book
+      src="https://64.media.tumblr.com/da03862b431de77130d762f80207a618/1df9bef7e1438380-e3/s1280x1920/c01adb52acf22897894a819787f7b3f8e1c46eea.jpg"
+    />
+    <Book
+      src="https://cdn.penguin.co.uk/dam-assets/books/9781529115543/9781529115543-jacket-large.jpg"
+    />
+    <Book
+      src="https://foxedquarterly.com/wp-content/uploads/2021/04/Clare-Chambers-Small-Pleasures.jpg"
+    />
+    <Book src="https://m.media-amazon.com/images/I/71SfSR3So3L.jpg" />
+  </div>
 </template>
