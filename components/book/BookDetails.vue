88eb848e33a95362e4c822176a9621766d4c236c
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-03-04 15:58:34 +0000

    UI – Extract data table component from book details flyout

--- components/book/BookDetails.vue
+++ components/book/BookDetails.vue
@@ -7,26 +7,12 @@
           {{ current_book.authors?.join(", ") }}
         </div>
       </div>
-      <div
-        @click="hide"
-        class="bg-slate-200 text-slate-500 text-xl rounded-full flex items-center justify-center h-10 w-10 cursor-pointer"
-      >
-        <Icon name="tabler:x"></Icon>
-      </div>
+      <CloseButton @click="hide" />
     </div>
     <div class="h-[400px] flex flex-row gap-8">
       <Book :book="current_book" class="h-full" />
       <div class="flex flex-col grow">
-        <div class="bg-white rounded-lg px-4 grid divide-y divide-slate-200">
-          <div v-for="item in data" class="flex flex-row gap-4 py-3">
-            <div class="w-24 text-slate-500">
-              {{ item.label }}
-            </div>
-            <div>
-              {{ item.value }}
-            </div>
-          </div>
-        </div>
+        <DataTable :data="data" class="bg-white" />
       </div>
     </div>
     <div class="my-12">{{ current_book.description }}</div>
@@ -34,28 +20,30 @@
 </template>
 
 <script setup lang="ts">
+import DataTable from "../utility/DataTable.vue";
+
 const { current_book, hide } = useBookFlyout();
 
 const data = [
   {
     label: "ISBN",
-    value: current_book.value?.isbn,
+    value: current_book.value?.isbn ?? "",
   },
   {
     label: "Author",
-    value: current_book.value?.authors?.join(", "),
+    value: current_book.value?.authors?.join(", ") ?? "",
   },
   {
     label: "Published",
-    value: current_book.value?.publishedDate,
+    value: current_book.value?.publishedDate ?? "",
   },
   {
     label: "Publisher",
-    value: current_book.value?.publisher,
+    value: current_book.value?.publisher ?? "",
   },
   {
     label: "Page Count",
-    value: current_book.value?.page_count,
+    value: current_book.value?.page_count?.toString() ?? "",
   },
 ];
 </script>
---
+++ components/utility/DataTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="rounded-lg px-4 grid divide-y divide-slate-200">
+    <div v-for="item in data" class="flex flex-row gap-4 py-3">
+      <div class="w-24 text-slate-500">
+        {{ item.label }}
+      </div>
+      <div>
+        {{ item.value }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  data: { label: string; value: string }[];
+}
+defineProps<Props>();
+</script>
